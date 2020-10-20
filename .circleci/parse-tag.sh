6640099ca17498ba203e549b77b5028026daae7d
mgrouch <mgrushinskiy@gmail.com>
2020-10-20 19:48:43 -0400

    ci build #1

--- .circleci/parse-tag.sh
+++ .circleci/parse-tag.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+  BASH_ENV=/tmp/.bash_env-5f8f6d969b9288123e04df41-0-build
+  CI=true
+  CIRCLECI=true
+  CIRCLE_BRANCH=hold-right-click
+  CIRCLE_BUILD_NUM=14
+  CIRCLE_BUILD_URL=https://circleci.com/gh/bareboat-necessities/evdev-right-click-emulation/14
+  CIRCLE_COMPARE_URL=
+  CIRCLE_JOB=build-arm64
+  CIRCLE_NODE_INDEX=0
+  CIRCLE_NODE_TOTAL=1
+  CIRCLE_PREVIOUS_BUILD_NUM=13
+  CIRCLE_PROJECT_REPONAME=evdev-right-click-emulation
+  CIRCLE_PROJECT_USERNAME=bareboat-necessities
+  CIRCLE_REPOSITORY_URL=git@github.com:bareboat-necessities/evdev-right-click-emulation.git
+  CIRCLE_SHA1=27a549c76c4baddc345414cb8fbca2ddcf1ccbcf
+  CIRCLE_SHELL_ENV=/tmp/.bash_env-5f8f6d969b9288123e04df41-0-build
+  CIRCLE_STAGE=build-arm64
+  CIRCLE_USERNAME=mgrouch
+  CIRCLE_WORKFLOW_ID=d3c40c47-b1b5-4a3e-8a99-1ad44d5e88ce
+  CIRCLE_WORKFLOW_JOB_ID=a0353f92-e4f9-4158-b7c7-fd199d38ec4e
+  CIRCLE_WORKFLOW_UPSTREAM_JOB_IDS=
+  CIRCLE_WORKFLOW_WORKSPACE_ID=d3c40c47-b1b5-4a3e-8a99-1ad44d5e88ce
+  CIRCLE_WORKING_DIRECTORY=~/project
+
+
 SEPARATOR=$(expr index "$CIRCLE_TAG" "/")
 NODE_NAME=${CIRCLE_TAG:0:$SEPARATOR-1}
 if expr index "$CIRCLE_TAG" "-" > /dev/null; then
