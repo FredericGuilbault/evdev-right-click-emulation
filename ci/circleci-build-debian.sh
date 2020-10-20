f6552f9ed135b643875e3c4627c3740bc15c29fa
mgrouch <mgrushinskiy@gmail.com>
2020-10-20 18:22:08 -0400

    ci build #1

--- ci/circleci-build-debian.sh
+++ ci/circleci-build-debian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y install libevdev2 libevdev-dev libinput-dev debhelper 
+sudo apt-get -y install libevdev2 libevdev-dev libinput-dev debhelper
 
 dpkg-buildpackage -rfakeroot -b -uc -us
 
