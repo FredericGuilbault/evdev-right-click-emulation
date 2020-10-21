cc79fc73a80b1f261c2de5bf440d6308dfe0a36e
mgrouch <mgrushinskiy@gmail.com>
2020-10-21 01:25:30 -0400

    ci build #1

--- .circleci/build-debian-armXX.sh
+++ .circleci/build-debian-armXX.sh
@@ -20,7 +20,7 @@ else
 fi
 
 docker run --privileged -d -ti -e "container=docker"  -v $(pwd):/ci-source:rw $DOCKER_IMAGE /bin/bash
-DOCKER_CONTAINER_ID=$(docker ps | grep $CONTAINER_DISTRO | awk '{print $1}')
+DOCKER_CONTAINER_ID=$(docker ps --last 8 | grep $CONTAINER_DISTRO | awk '{print $1}')
 
 docker exec -ti $DOCKER_CONTAINER_ID apt-get update
 docker exec -ti $DOCKER_CONTAINER_ID apt-get -y install libevdev2 libevdev-dev libinput-dev debhelper
