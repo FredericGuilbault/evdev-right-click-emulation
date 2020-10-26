c35ff2f9d77cbd952f7653c7f0ce642330933447
mgrouch <mgrushinskiy@gmail.com>
2020-10-25 20:58:43 -0400

    ci build #1

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -9,7 +9,7 @@ set -xe
 
 DOCKER_SOCK="unix:///var/run/docker.sock"
 
-echo "DOCKER_OPTS=\"-H tcp://127.0.0.1:2375 -H $DOCKER_SOCK -s devicemapper\"" | sudo tee /etc/default/docker > /dev/null
+echo "DOCKER_OPTS=\"-H tcp://127.0.0.1:2375 -H $DOCKER_SOCK -s overlay2\"" | sudo tee /etc/default/docker > /dev/null
 sudo service docker restart
 sleep 5;
 
@@ -17,22 +17,23 @@ if [ "$EMU" = "on" ]; then
   if [ "$CONTAINER_DISTRO" = "raspbian" ]; then
       docker run --rm --privileged multiarch/qemu-user-static:register --reset
   else
-      docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+      docker run --rm --privileged --cap-add=ALL --security-opt="seccomp=unconfined" multiarch/qemu-user-static --reset --credential yes --persistent yes
   fi
 fi
 
 WORK_DIR=$(pwd):/ci-source
 
-docker run --privileged -d -ti -e "container=docker"  -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
+docker run --privileged --cap-add=ALL --security-opt="seccomp=unconfined" -d -ti -e "container=docker"  -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
 DOCKER_CONTAINER_ID=$(docker ps --last 4 | grep $CONTAINER_DISTRO | awk '{print $1}')
 
 docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get update
-docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install libevdev2 libevdev-dev libinput-dev dh-exec
+docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install apt-transport-https wget curl gnupg2
+docker exec -ti $DOCKER_CONTAINER_ID apt-get -y install dpkg-dev debhelper devscripts equivs pkg-config apt-utils fakeroot
+docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install libevdev2 libevdev-dev libinput-dev dh-exec autotools-dev autoconf
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
+    "update-alternatives --set fakeroot /usr/bin/fakeroot-tcp; cd ci-source; dpkg-buildpackage -b -uc -us -j4; mkdir dist; mv ../*.deb dist; chmod -R a+rw dist"
 
-docker exec --privileged -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
-    "cd ci-source; dpkg-buildpackage -b -uc -us; mkdir dist; mv ../*.deb dist; chmod -R a+rw dist"
-
-find dist -name \*.deb
+find dist -name \*.\*$EXT
 
 echo "Stopping"
 docker ps -a
